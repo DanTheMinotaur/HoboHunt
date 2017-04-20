90d6f6e5281b2864180ebed4092ff34d694cdc35
x14130114 <x14130114@student.ncirl.ie>
2017-04-20 22:12:21 +0100

    MenuAndScore

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    void OnMouseUp()
+    {
+        Application.LoadLevel("scene");
+    }
+}
